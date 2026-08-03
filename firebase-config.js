f8dc03456dbce428a705f83b10e6eb21747fdfc2
briseida16 <briseidabatalla@gmail.com>
2026-08-03 15:44:59 +0200

    Connect wardrobe to Firestore

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCThGwBnZfhIeN9T1woIjJH5JFiBWc7fkQ",
+  authDomain: "armario---bricloset.firebaseapp.com",
+  projectId: "armario---bricloset",
+  storageBucket: "armario---bricloset.firebasestorage.app",
+  messagingSenderId: "34156505129",
+  appId: "1:34156505129:web:54d67023e9994cc6409438"
+};
