1ba5ec511ff2288841da426f8784ce5185564501
briseida16 <briseidabatalla@gmail.com>
2026-09-05 23:44:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,47 @@
-const CACHE = 'bricloset-v4';
-const ARCHIVOS = ['./', './index.html', './manifest.json'];
+const CACHE = 'bricloset-v5';
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ARCHIVOS))
+const ARCHIVOS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))
   );
+
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
+self.addEventListener('activate', event => {
+  event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
     )
   );
+
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .catch(() => caches.match(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (event.request.method === 'GET' && response.ok) {
+          const responseClone = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
